25c0683695ae6d4257b8e905259cbafad022a7aa
Vectura <vectura1986@gmail.com>
2023-11-14 05:22:15 +0300

    binary_search

--- exercises.py
+++ exercises.py
@@ -1,2 +1,49 @@
-name = input("Enter your name: ")
-print(f"Hello, {name}!!!")
+import math
+
+# name = input("Enter your name: ")
+# print(f"Hello, {name}!!!")
+
+# print("What do you call a bear with no teeth?\nA gummy bear!")
+
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+# total = num1 + num2
+# print(f"The total is {total}")
+
+
+
+# days = int(input("How many days?: "))
+# hours = days*24
+# minutes = hours*60
+# sec = minutes*60
+# print(f"{days} is {hours} hours, {minutes} minutes and {sec} seconds")
+
+# num1 = int(input("Enter a number more then 100: "))
+# num2 = int(input("Enter a number less then 10: "))
+# res = num1//num2
+# print(res)
+
+# rain = input("Is it rainy?: ")
+# wind = input("Is it windy?: ")
+# rain = str.lower(rain)
+# wind = str.lower(wind)
+# if rain == "yes" and wind == "yes":
+#     print("Its too windy for umbrella")
+# elif rain == "yes" and wind != "yes":
+#     print("Take an umbrella")
+# else:
+#     print("Enjoy your day!")
+
+# age = int(input("How old are you?: "))
+# if age > 18:
+#     print("You can vote")
+# elif age == 17:
+#     print("You can learn drive")
+# elif age == 16:
+#     print("You can buy a lottery ticket")
+# else:
+#     print("You can go Trick-or-Treating")
+
+
+
+# print(round(math.pi, 5))
