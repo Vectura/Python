b98a0c69ece948a15848e3b7ba5e2e366857d4d5
Vectura <vectura1986@gmail.com>
2023-12-07 01:34:44 +0300

    задачки по рекурсии

--- recursion.py
+++ recursion.py
@@ -1,4 +1,4 @@
-
+# факториал числа
 # def fact(x):
 #     if x == 1:
 #         return 1
@@ -7,16 +7,37 @@
 
 # print(fact(6))
 
+# Сумма элементов массива
+# def summArr(x, n):
+#     if n == 0:
+#         return 0
+#     return x[n-1] + summArr(x, n-1)
 
-def summArr(x, n):
-    if n == 0:
-        return 0
-    return x[n-1] + summArr(x, n-1)
+# Поиск максимального значения 
+# def Search(x, n):
+#     if n == 1:
+#         return x[0]
+#     elif x[0] <= x[n-1]:
+#          x[0] = x[n-1]
+#          return Search(x[0:-1], n-1)
+#     elif x[0] >= x[n-1]:
+#          return Search(x[0:-1], n-1)
+    
+def multiTable(x, n):
+    if n == 1:
+        total = x[0] * 2
+        print(total)
+    elif n > 0:
+        total = x[0] * x[n-1]
+        print(total)
+        return multiTable(x[0:-1], n-1)
 
-n = int(input("Enter lenght arr: "))
+n = int(input("Enter lenght list: "))
 m = []
 for i in range(0, n):
-    item = int(input("Enter item arr: "))
+    item = int(input("Enter item list: "))
     m.append(item)
-print(m)
-print("Cумма элементов массива равна: ", summArr(m, n))
+print(m) 
+multiTable(m, n)
+
+
